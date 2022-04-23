c689bd169c91095d936cf6ab2e0b8cf91c74e8fc
trickstarcandina <rinnegan101220@gmail.com>
2022-04-23 16:32:46 +0700

    update time cron tab

--- src/shedules/index.js
+++ src/shedules/index.js
@@ -10,10 +10,10 @@ exports.InitCron = async function (client) {
 		// cron.schedule('*/30 * * * * *', async () => {}, {
 		// 	scheduled: true
 		// });
-		//backup lottery At minute 0 past every 2nd hour
+		//backup lottery At :30 in every 2nd hour from 1am through 11pm
 		cron.schedule(
 			// '* * * * *',
-			'0 */2 * * *',
+			'30 1-23/2 * * *',
 			async () => {
 				await lotteryShedulesBackupLotteryArray(client);
 				//clear cooldown
