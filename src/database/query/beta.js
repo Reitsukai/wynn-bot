8f9f9409fc7e0a34972cdd1349753f8962ffd7ad
Trịnh Minh Thắng <61593963+trickstarcandina@users.noreply.github.com>
2022-06-02 15:46:17 +0700

    Revert "Config"

--- src/database/query/beta.js
+++
@@ -1,9 +0,0 @@
-const betabotShema = require('../schema/betabot');
-
-module.exports.getBetaBotInfo = async function (key) {
-	return await betabotShema.findOne({ discordId: key });
-};
-
-module.exports.setBetaBotInfo = async function (key, fieldUpdate) {
-	return await betabotShema.updateOne({ discordId: key }, fieldUpdate, { upsert: true });
-};
