6bac5e28c9e03d736c53d7e0f6b2f1c10b19841a
trickstarcandina <rinnegan101220@gmail.com>
2022-06-14 16:58:45 +0700

    config: rate fishing

--- src/config/collect.js
+++ src/config/collect.js
@@ -11,19 +11,19 @@ module.exports = {
 			{ rate: 1, name: 'fail' }
 		],
 		river: [
-			{ rate: 0.5, name: 'frog' },
-			{ rate: 0.75, name: 'carp' },
+			{ rate: 0.6, name: 'frog' },
+			{ rate: 0.8, name: 'carp' },
 			{ rate: 0.85, name: 'crocodile' },
 			{ rate: 1, name: 'fail' }
 		],
 		sea: [
-			{ rate: 0.55, name: 'eel' },
-			{ rate: 0.7, name: 'squid' },
-			{ rate: 0.76, name: 'shark' },
-			{ rate: 0.79, name: 'crocodile' },
-			{ rate: 0.8, name: 'whale' },
-			{ rate: 0.80005, name: 'kraken' },
-			{ rate: 0.800051, name: 'mermaid' },
+			{ rate: 0.45, name: 'eel' },
+			{ rate: 0.77, name: 'squid' }, // 0.32
+			{ rate: 0.785, name: 'shark' }, // 0.015
+			{ rate: 0.795, name: 'crocodile' }, // 0.01
+			{ rate: 0.8, name: 'whale' }, // 0.005
+			{ rate: 0.800005, name: 'kraken' },
+			{ rate: 0.8000051, name: 'mermaid' },
 			{ rate: 1, name: 'fail' }
 		]
 	}
