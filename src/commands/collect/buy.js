519094a28da29c5981dbd11ef70d5ab95e114f87
trickstarcandina <rinnegan101220@gmail.com>
2022-06-26 10:53:54 +0700

    refactor: add try catch to command

--- src/commands/collect/buy.js
+++ src/commands/collect/buy.js
@@ -22,103 +22,108 @@ class UserCommand extends WynnCommand {
 	}
 
 	async messageRun(message, args) {
-		let isBlock = await this.container.client.db.checkIsBlock(message.author.id);
-		if (isBlock === true) return;
-		if (this.container.client.options.spams.get(`${message.author.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
-			return await reminderCaptcha(message, this.container.client, message.author.id, message.author.tag);
-		}
-		const t = await fetchT(message);
-		const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(message.author.id, this.name, coolDown.collect.buy, t);
-		if (checkCoolDown) {
-			if (checkCoolDown.image !== undefined) {
-				return await utils.sendCaptchaImage(
-					message.author.id,
-					this.container.client,
-					checkCoolDown.image,
-					checkCoolDown.text,
-					message,
-					t('commands/captcha:require', {
-						user: message.author.tag
-					})
-				);
-			}
-			return send(message, checkCoolDown);
-		}
-		let input1 = await args.next();
-		if (input1 === 'bait') {
-			let userInfo = await this.container.client.db.fetchUser(message.author.id);
-			let input2 = await args.next();
-			if (input2 === null || isNaN(input2)) {
-				input2 = 1;
-			}
-			return await this.buyBait(message, userInfo, t, input2, message.author.tag);
-		}
-		return;
-	}
-
-	async buyBait(message, userInfo, t, amount, tag) {
 		try {
-			if (userInfo.money - collect.fishing.buy * amount < 0) {
-				return await utils.returnSlashAndMessage(
-					message,
-					t('commands/buy:nomoney', {
-						user: tag
-					})
-				);
+			let isBlock = await this.container.client.db.checkIsBlock(message.author.id);
+			if (isBlock === true) return;
+			if (this.container.client.options.spams.get(`${message.author.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
+				return await reminderCaptcha(message, this.container.client, message.author.id, message.author.tag);
 			}
-			await Promise.all([
-				this.container.client.db.updateUser(userInfo.discordId, {
-					$inc: {
-						money: -collect.fishing.buy * amount
-					}
-				}),
-				this.container.client.db.updateItemFish(userInfo.discordId, {
-					$inc: {
-						bait: amount === 0 ? 1 : amount
-					}
-				})
-			]);
-			return await utils.returnSlashAndMessage(
-				message,
-				t('commands/buy:baitdone', {
-					user: tag,
-					amount: amount
-				})
-			);
+			const t = await fetchT(message);
+			const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(message.author.id, this.name, coolDown.collect.buy, t);
+			if (checkCoolDown) {
+				if (checkCoolDown.image !== undefined) {
+					return await utils.sendCaptchaImage(
+						message.author.id,
+						this.container.client,
+						checkCoolDown.image,
+						checkCoolDown.text,
+						message,
+						t('commands/captcha:require', {
+							user: message.author.tag
+						})
+					);
+				}
+				return send(message, checkCoolDown);
+			}
+			let input1 = await args.next();
+			if (input1 === 'bait') {
+				let userInfo = await this.container.client.db.fetchUser(message.author.id);
+				let input2 = await args.next();
+				if (input2 === null || isNaN(input2)) {
+					input2 = 1;
+				}
+				return await this.buyBait(message, userInfo, t, input2, message.author.tag);
+			}
+			return;
 		} catch (err) {
 			logger.error(err);
 			return await send(message, t('other:error', { supportServer: process.env.SUPPORT_SERVER_LINK }));
 		}
 	}
 
+	async buyBait(message, userInfo, t, amount, tag) {
+		if (userInfo.money - collect.fishing.buy * amount < 0) {
+			return await utils.returnSlashAndMessage(
+				message,
+				t('commands/buy:nomoney', {
+					user: tag
+				})
+			);
+		}
+		await Promise.all([
+			this.container.client.db.updateUser(userInfo.discordId, {
+				$inc: {
+					money: -collect.fishing.buy * amount
+				}
+			}),
+			this.container.client.db.updateItemFish(userInfo.discordId, {
+				$inc: {
+					bait: amount === 0 ? 1 : amount
+				}
+			})
+		]);
+		return await utils.returnSlashAndMessage(
+			message,
+			t('commands/buy:baitdone', {
+				user: tag,
+				amount: amount
+			})
+		);
+	}
+
 	async execute(interaction) {
-		let isBlock = await this.container.client.db.checkIsBlock(interaction.user.id);
-		if (isBlock === true) return;
-		if (this.container.client.options.spams.get(`${interaction.user.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
-			return await reminderCaptcha(interaction, this.container.client, interaction.user.id, interaction.user.tag);
-		}
-		const t = await fetchT(interaction);
-		const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(interaction.user.id, this.name, coolDown.collect.buy, t);
-		if (checkCoolDown) {
-			if (checkCoolDown.image !== undefined) {
-				return await utils.sendCaptchaImage(
-					interaction.user.id,
-					this.container.client,
-					checkCoolDown.image,
-					checkCoolDown.text,
-					interaction,
-					t('commands/captcha:require', {
-						user: interaction.user.tag
-					})
-				);
+		try {
+			let isBlock = await this.container.client.db.checkIsBlock(interaction.user.id);
+			if (isBlock === true) return;
+			if (this.container.client.options.spams.get(`${interaction.user.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
+				return await reminderCaptcha(interaction, this.container.client, interaction.user.id, interaction.user.tag);
 			}
-			return await interaction.reply(checkCoolDown);
+			const t = await fetchT(interaction);
+			const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(interaction.user.id, this.name, coolDown.collect.buy, t);
+			if (checkCoolDown) {
+				if (checkCoolDown.image !== undefined) {
+					return await utils.sendCaptchaImage(
+						interaction.user.id,
+						this.container.client,
+						checkCoolDown.image,
+						checkCoolDown.text,
+						interaction,
+						t('commands/captcha:require', {
+							user: interaction.user.tag
+						})
+					);
+				}
+				return await interaction.reply(checkCoolDown);
+			}
+			if (interaction.options.getSubcommand() === 'bait') {
+				let userInfo = await this.container.client.db.fetchUser(interaction.user.id);
+				return await this.buyBait(interaction, userInfo, t, Number(interaction.options.getInteger('amount')), interaction.user.tag);
+			}
+			return;
+		} catch (err) {
+			logger.error(err);
+			return await send(message, t('other:error', { supportServer: process.env.SUPPORT_SERVER_LINK }));
 		}
-		if (interaction.options.getSubcommand() === 'bait') {
-			let userInfo = await this.container.client.db.fetchUser(interaction.user.id);
-			return await this.buyBait(interaction, userInfo, t, Number(interaction.options.getInteger('amount')), interaction.user.tag);
-		}
-		return;
 	}
 }
 
--- src/commands/collect/fishing.js
+++ src/commands/collect/fishing.js
@@ -22,121 +22,126 @@ class UserCommand extends WynnCommand {
 	}
 
 	async messageRun(message, args) {
-		let isBlock = await this.container.client.db.checkIsBlock(message.author.id);
-		if (isBlock === true) return;
-		if (this.container.client.options.spams.get(`${message.author.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
-			return await reminderCaptcha(message, this.container.client, message.author.id, message.author.tag);
-		}
-		const t = await fetchT(message);
-		const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(message.author.id, this.name, coolDown.collect.fishing, t);
-		if (checkCoolDown) {
-			if (checkCoolDown.image !== undefined) {
-				return await utils.sendCaptchaImage(
-					message.author.id,
-					this.container.client,
-					checkCoolDown.image,
-					checkCoolDown.text,
-					message,
-					t('commands/captcha:require', {
-						user: message.author.tag
-					})
-				);
-			}
-			return send(message, checkCoolDown);
-		}
-		let input1 = await args.next();
-		if (input1 === 'config') {
-			let input2 = await args.next();
-			if (['tub', 'lake', 'river', 'sea'].includes(input2)) {
-				return await Promise.all([
-					this.container.client.resetCustomCooldown(message.author.id, this.name),
-					this.configLocation(message, t, message.author.id, input2, message.author.tag)
-				]);
-			}
-			// sai type return ...;
-		}
-		return await this.mainProcess(message, t, message.author.id, message.author.tag);
-	}
-
-	async mainProcess(message, t, userId, tag) {
 		try {
-			const itemFish = await this.container.client.db.getItemFishByDiscordId(userId);
-			if (itemFish.bait < 1) {
-				return await utils.returnSlashAndMessage(
-					message,
-					t('commands/fishing:nobait', {
-						user: tag,
-						prefix: await this.container.client.fetchPrefix(message)
-					})
-				);
+			let isBlock = await this.container.client.db.checkIsBlock(message.author.id);
+			if (isBlock === true) return;
+			if (this.container.client.options.spams.get(`${message.author.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
+				return await reminderCaptcha(message, this.container.client, message.author.id, message.author.tag);
 			}
-			const locationFishing = collect.fishing;
-			let map = new Map();
-			map.set('tub', locationFishing.tub);
-			map.set('lake', locationFishing.lake);
-			map.set('river', locationFishing.river);
-			map.set('sea', locationFishing.sea);
-			let random = Math.random();
-			let resultFishing = '';
-			let arrayRate = map.get(itemFish.location);
-			for (let i = 0; i < arrayRate.length; i++) {
-				if (random <= arrayRate[i].rate) {
-					resultFishing = arrayRate[i].name;
-					break;
-				}
-			}
-			if (resultFishing === 'fail') {
-				await this.container.client.db.updateItemFish(userId, {
-					$inc: {
-						bait: -1
-					}
-				});
-				return await utils.returnSlashAndMessage(
-					message,
-					t('commands/fishing:fishingfail', {
-						user: tag
-					})
-				);
-			}
-			let fishReceive = await this.container.client.db.getFishByName(resultFishing);
-			let newArray = itemFish.arrayFish.slice();
-			let flag = 0;
-			for (let i = 0; i < newArray.length; i++) {
-				if (newArray[i].id === fishReceive.id) {
-					newArray[i].amount += 1;
-					flag = 1;
-					break;
-				}
-			}
-			if (flag === 0) {
-				newArray.push({
-					id: fishReceive.id,
-					name: fishReceive.name,
-					emoji: fishReceive.emoji,
-					amount: 1
-				});
-			}
-
-			await this.container.client.db.updateItemFish(userId, {
-				$inc: {
-					bait: -1
-				},
-				arrayFish: newArray
-			});
-			return await utils.returnSlashAndMessage(
-				message,
-				// 'Tính năng đang bảo trì tạm thời'
-				t('commands/fishing:fishingdone', {
-					user: tag,
-					name: collect.fishing.special.includes(fishReceive.name) ? `${fishReceive.name}` : t(`commands/fishing:${fishReceive.name}`),
-					emoji: fishReceive.emoji,
-					rarity: t(`commands/fishing:${fishReceive.rarity}`)
-				})
+			const t = await fetchT(message);
+			const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(
+				message.author.id,
+				this.name,
+				coolDown.collect.fishing,
+				t
 			);
+			if (checkCoolDown) {
+				if (checkCoolDown.image !== undefined) {
+					return await utils.sendCaptchaImage(
+						message.author.id,
+						this.container.client,
+						checkCoolDown.image,
+						checkCoolDown.text,
+						message,
+						t('commands/captcha:require', {
+							user: message.author.tag
+						})
+					);
+				}
+				return send(message, checkCoolDown);
+			}
+			let input1 = await args.next();
+			if (input1 === 'config') {
+				let input2 = await args.next();
+				if (['tub', 'lake', 'river', 'sea'].includes(input2)) {
+					return await Promise.all([
+						this.container.client.resetCustomCooldown(message.author.id, this.name),
+						this.configLocation(message, t, message.author.id, input2, message.author.tag)
+					]);
+				}
+				// sai type return ...;
+			}
+			return await this.mainProcess(message, t, message.author.id, message.author.tag);
 		} catch (err) {
 			logger.error(err);
 			return await send(message, t('other:error', { supportServer: process.env.SUPPORT_SERVER_LINK }));
 		}
+	}
+
+	async mainProcess(message, t, userId, tag) {
+		const itemFish = await this.container.client.db.getItemFishByDiscordId(userId);
+		if (itemFish.bait < 1) {
+			return await utils.returnSlashAndMessage(
+				message,
+				t('commands/fishing:nobait', {
+					user: tag,
+					prefix: await this.container.client.fetchPrefix(message)
+				})
+			);
+		}
+		const locationFishing = collect.fishing;
+		let map = new Map();
+		map.set('tub', locationFishing.tub);
+		map.set('lake', locationFishing.lake);
+		map.set('river', locationFishing.river);
+		map.set('sea', locationFishing.sea);
+		let random = Math.random();
+		let resultFishing = '';
+		let arrayRate = map.get(itemFish.location);
+		for (let i = 0; i < arrayRate.length; i++) {
+			if (random <= arrayRate[i].rate) {
+				resultFishing = arrayRate[i].name;
+				break;
+			}
+		}
+		if (resultFishing === 'fail') {
+			await this.container.client.db.updateItemFish(userId, {
+				$inc: {
+					bait: -1
+				}
+			});
+			return await utils.returnSlashAndMessage(
+				message,
+				t('commands/fishing:fishingfail', {
+					user: tag
+				})
+			);
+		}
+		let fishReceive = await this.container.client.db.getFishByName(resultFishing);
+		let newArray = itemFish.arrayFish.slice();
+		let flag = 0;
+		for (let i = 0; i < newArray.length; i++) {
+			if (newArray[i].id === fishReceive.id) {
+				newArray[i].amount += 1;
+				flag = 1;
+				break;
+			}
+		}
+		if (flag === 0) {
+			newArray.push({
+				id: fishReceive.id,
+				name: fishReceive.name,
+				emoji: fishReceive.emoji,
+				amount: 1
+			});
+		}
+
+		await this.container.client.db.updateItemFish(userId, {
+			$inc: {
+				bait: -1
+			},
+			arrayFish: newArray
+		});
+		return await utils.returnSlashAndMessage(
+			message,
+			// 'Tính năng đang bảo trì tạm thời'
+			t('commands/fishing:fishingdone', {
+				user: tag,
+				name: collect.fishing.special.includes(fishReceive.name) ? `${fishReceive.name}` : t(`commands/fishing:${fishReceive.name}`),
+				emoji: fishReceive.emoji,
+				rarity: t(`commands/fishing:${fishReceive.rarity}`)
+			})
+		);
 	}
 
 	async configLocation(message, t, userId, type, tag) {
@@ -153,32 +158,48 @@ class UserCommand extends WynnCommand {
 	}
 
 	async execute(interaction) {
-		let isBlock = await this.container.client.db.checkIsBlock(interaction.user.id);
-		if (isBlock === true) return;
-		if (this.container.client.options.spams.get(`${interaction.user.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
-			return await reminderCaptcha(interaction, this.container.client, interaction.user.id, interaction.user.tag);
-		}
-		const t = await fetchT(interaction);
-		const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(interaction.user.id, this.name, coolDown.collect.fishing, t);
-		if (checkCoolDown) {
-			if (checkCoolDown.image !== undefined) {
-				return await utils.sendCaptchaImage(
-					interaction.user.id,
-					this.container.client,
-					checkCoolDown.image,
-					checkCoolDown.text,
+		try {
+			let isBlock = await this.container.client.db.checkIsBlock(interaction.user.id);
+			if (isBlock === true) return;
+			if (this.container.client.options.spams.get(`${interaction.user.id}`) === 'warn' || (isBlock.length > 0 && !isBlock[0].isResolve)) {
+				return await reminderCaptcha(interaction, this.container.client, interaction.user.id, interaction.user.tag);
+			}
+			const t = await fetchT(interaction);
+			const checkCoolDown = await this.container.client.checkTimeCoolDownWithCheckSpam(
+				interaction.user.id,
+				this.name,
+				coolDown.collect.fishing,
+				t
+			);
+			if (checkCoolDown) {
+				if (checkCoolDown.image !== undefined) {
+					return await utils.sendCaptchaImage(
+						interaction.user.id,
+						this.container.client,
+						checkCoolDown.image,
+						checkCoolDown.text,
+						interaction,
+						t('commands/captcha:require', {
+							user: interaction.user.tag
+						})
+					);
+				}
+				return await interaction.reply(checkCoolDown);
+			}
+			if (interaction.options.getSubcommand() === 'config') {
+				return await this.configLocation(
 					interaction,
-					t('commands/captcha:require', {
-						user: interaction.user.tag
-					})
+					t,
+					interaction.user.id,
+					interaction.options.getString('location'),
+					interaction.user.tag
 				);
 			}
-			return await interaction.reply(checkCoolDown);
+			return await this.mainProcess(interaction, t, interaction.user.id, interaction.user.tag);
+		} catch (err) {
+			logger.error(err);
+			return await send(message, t('other:error', { supportServer: process.env.SUPPORT_SERVER_LINK }));
 		}
-		if (interaction.options.getSubcommand() === 'config') {
-			return await this.configLocation(interaction, t, interaction.user.id, interaction.options.getString('location'), interaction.user.tag);
-		}
-		return await this.mainProcess(interaction, t, interaction.user.id, interaction.user.tag);
 	}
 }
 
