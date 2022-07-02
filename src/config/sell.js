366038929e9ab6d35f94560459861d8a58fd1e49
trickstarcandina <rinnegan101220@gmail.com>
2022-07-02 11:07:06 +0700

    config: add new fish

--- src/config/sell.js
+++ src/config/sell.js
@@ -1,13 +1,18 @@
 module.exports = {
 	fishing: [
 		{ name: 'frog', price: 100 },
-		{ name: 'eel', price: 120 },
+		{ name: 'eel', price: 200 },
 		{ name: 'carp', price: 2000 },
-		{ name: 'shark', price: 7500 },
-		{ name: 'whale', price: 25000 },
-		{ name: 'crocodile', price: 10000 },
-		{ name: 'squid', price: 350 },
+		{ name: 'shark', price: 10000 },
+		{ name: 'whale', price: 30000 },
+		{ name: 'crocodile', price: 15000 },
+		{ name: 'squid', price: 450 },
 		{ name: 'kraken', price: 500000 },
-		{ name: 'mermaid', price: 4000000 }
+		{ name: 'mermaid', price: 4000000 },
+		{ name: 'ray', price: 300 },
+		{ name: 'crab', price: 800 },
+		{ name: 'shrimp', price: 500 },
+		{ name: 'jellyfish', price: 700 },
+		{ name: 'dolphin', price: 8000 }
 	]
 };
